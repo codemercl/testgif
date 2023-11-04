341b31610da36cf2fc3eb95f9058268d6167f49f
codemercl <developer.venium@gmail.com>
2023-11-04 09:32:51 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/giffy/sw.js', { scope: '/giffy/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/testgif/sw.js', { scope: '/testgif/' })})}
